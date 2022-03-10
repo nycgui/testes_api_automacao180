3b8ced5da6ea9b00084cde8bb4ed0eebe87560dd
nycgui <nyc.gui@gmail.com>
2022-03-10 05:39:25 -0300

    cenario listar todos produtos

--- spec/specs/get_equipo_spec.rb
+++ spec/specs/get_equipo_spec.rb
@@ -1,11 +1,13 @@
 describe 'GET /equipos/{equipo_id}' do
+
   before(:all) do
     payload = { email:'nyc.gui@rozinmaill.com', password: '123456' }
     result = Sessions.new.login(payload)
     @user_id = result.parsed_response['_id']
   end
- 
+
   context 'buscar um equipo' do
+
     before(:all) do
       @payload = {
         thumbnail: Helpers::get_thumb('clarinete.jpg'),
@@ -41,4 +43,57 @@ describe 'GET /equipos/{equipo_id}' do
     end
   end
 
+end
+
+describe 'GET /equipos' do
+
+  before(:all) do
+    payload = {email: 'gui@gmail.com', password: '123456'}
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response['_id']
+  end
+
+  context 'obter uma lista de equipamentos' do
+
+    before(:all) do
+      #Dado que eu tenha varios equipos cadastrados
+      payloads = [
+        {
+        thumbnail: Helpers::get_thumb('clarinete.jpg'),
+        name: 'Clarinete',
+        category: 'Sopro',
+        price: 400
+        },
+        {
+          thumbnail: Helpers::get_thumb('kramer.jpg'),
+          name: 'Kramer',
+          category: 'Cordas',
+          price: 250
+        },
+        {
+          thumbnail: Helpers::get_thumb('amp.jpg'),
+          name: 'Amp',
+          category: 'Cordas',
+          price: 170
+        }
+      ]
+      payloads.each do |payload|
+        MongoDB.new.remove_equipo(payload[:name], @user_id)
+        Equipos.new.create(payload, @user_id)
+      end
+      # Quando procuro todos os produto
+
+      @result = Equipos.new.list(@user_id)
+    end
+
+    it 'deve retornar 200' do
+      expect(@result.code).to eql 200
+    end
+
+    it 'nao deve ser vazio' do
+      expect(@result.parsed_response).not_to be_empty
+      expect(@result.parsed_response.class).to eql Array
+    end
+
+  end
 end
