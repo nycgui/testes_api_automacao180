44dc700aee8b52865da9d164e27f684d77fed8cb
nycollas_nogueira <nyc.gui@gmail.com>
2022-10-17 21:40:29 -0300

    metodos do dashboard

--- web/features/support/views/pages/dash_page.rb
+++ web/features/support/views/pages/dash_page.rb
@@ -8,4 +8,25 @@ class DashPage
   def goto_equipo_form
     click_button 'Criar anúncio'
   end
+
+  def remove_equipo(name)
+    equipo = find('.equipo-list li', text: name)
+    equipo.find('.delete-icon').click
+  end
+
+  def confirm_removal(value)
+    click_on value
+  end
+
+  def has_no_equipo?(name)
+    page.has_no_css?('.equipo-list li', text: name)
+  end
+
+  def order
+    find(".notifications p")
+  end
+
+  def order_actions(action)
+      page.has_css?('.notifications button', text: action)
+  end
 end
